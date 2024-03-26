314ba0dfd48bdc2af4792dc925ef5e935a698a8a
Jason Hao <jyhao1998@gmail.com>
2024-03-26 11:49:02 -0700

    feat: add profile section

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -6,7 +6,6 @@ import { Database } from "../types/supabase";
 export default async function createSupabaseServer() {
   const cookieStore = cookies();
 
-  console.log("Create supabase server");
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
